d1f1258ea90f9785a4795d9fe067019cf0f209cf
SeanReedy1 <sean.reedy1@gmail.com>
2020-08-31 09:52:27 -0500

    pushing to github

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express= require('express')
+const morgan=require('morgan')
+const cors=require('cors')
+const helmet=require('helmet')
+const MOVIES = require('./movies-data.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use(cors())
+app.use(helmet())
+
+const PORT = 8000
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const bearerToken=req.get('Authorization');
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    } 
+
+    next();
+}
+)
+
+app.get('/movie', function handleGetMovies(req, res) {
+    let response=MOVIES;
+    
+    if(req.query.genre) {
+        response=response.filter(movie => {
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        })
+    }
+
+    if(req.country) {
+        response=response.filter(movie => {
+            movie.country.toLowerCase().includes(req.query.genre.toLowerCase())
+        })
+    }
+
+    if(req.query.avg_vote) {
+        response=response.filter(movie => {
+            Number(movie.avg_vote) > Number(req.query.avg_vote)
+        })
+    }
+
+    res.json(response)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on Port http://localhost:${PORT}`)
+})
